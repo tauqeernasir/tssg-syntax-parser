084296fba245ac12b9bfe007992a891eeec7045e
Tauqeer Nasir <tauqeer.insta@gmail.com>
2020-10-30 03:46:42 +0400

    feat: add support for basic mongoose model

--- src/addons/mongoose/mongoose-transformer.js
+++ src/addons/mongoose/mongoose-transformer.js
@@ -1,16 +1,32 @@
-const { readFileSync } = require("fs");
-const path = require("path");
-const { parser } = require("../../transformer");
+/**
+ * Generate Mongoose Schemas from given OAS3
+ */
 
-// const mongooseSchemaTmpl = readFileSync(
-//   path.resolve(__dirname, "./mongoose-schema.tmpl"),
-//   { encoding: "utf-8" }
-// );
+const mongooseImportTmpl = `
+import { Schema, model } from 'mongoose';
+
+`;
 
 const mongooseSchemaTmpl = `
-const {{schema_name}} = mongoose.Schema({
+/**
+ * @schema {{schema_name}}
+ *
+ */
+const {{schema_name}}Schema = new mongoose.Schema({
   {{schema_props}}
 });
+
+export const {{schema_name}}Model = mongoose.model('{{schema_name}}', {{schema_name}}Schema);
+
+// ===================
+`;
+
+const mongoosePropTmpl = `
+  {{prop_name}}: {{props}},
+`;
+
+const mongooseKeyValTmpl = `
+    {{key}}: {{value}},
 `;
 
 const replaceInTemplate = function (template, data) {
@@ -18,64 +34,87 @@ const replaceInTemplate = function (template, data) {
   return template.replace(pattern, (_, token) => data[token] || "");
 };
 
-const str = `
-  Schemas {
-    User {
-      name: string,
-      age: number
-    }
+const lowerCaseFirst = (str) => {
+  return str
+    .split("")
+    .map((char, index) => {
+      if (index === 0) {
+        return char.toLowerCase();
+      }
+      return char;
+    })
+    .join("");
+};
 
-    Employee {
-      role: string,
-      age: boolean,
-      salary: number,
-    }
-  }`;
-
-const parsedSpec = parser(str);
-
-console.log(JSON.stringify(parsedSpec, null, 2));
-
-mongooseTransformer(parsedSpec);
-
+// Entry point
 function mongooseTransformer(spec) {
   const { schemas } = spec;
 
   const schemaKeys = Object.keys(schemas);
 
-  let mongooseSchemas = ``;
+  let generatedMongooseCode = mongooseImportTmpl;
   for (const key of schemaKeys) {
-    mongooseSchemas += replaceInTemplate(mongooseSchemaTmpl, {
-      schema_name: key,
+    generatedMongooseCode += replaceInTemplate(mongooseSchemaTmpl, {
+      schema_name: lowerCaseFirst(key),
       schema_props: processSchema(schemas[key]),
     });
   }
 
-  console.log(mongooseSchemas);
+  return generatedMongooseCode;
 }
 
+// only proecess top-level schema
 function processSchema(schema) {
   const propKeys = Object.keys(schema.properties);
-  console.log("asdf", propKeys);
+  console.log("asdf", schema);
 
   let cachedProps = "";
   for (const propKey of propKeys) {
     const { type } = schema.properties[propKey];
-    cachedProps += `
-        ${propKey}: {
-          type: ${getType(type)},
-        },
-    `;
+    cachedProps += replaceInTemplate(mongoosePropTmpl, {
+      prop_name: propKey,
+      props: processProp(type, schema.properties[propKey]),
+    });
   }
 
   return cachedProps;
 }
 
+// process only props `key: value` pair
+// also handle `values` as sub-schemas to generate nested schema output
+function processProp(type, schema) {
+  if (type === "object") {
+    return `{
+      ${processSchema(schema)}
+    }
+    `;
+  }
+
+  if (type === "array") {
+    return `[
+      ${processProp(schema.items.type, schema.items)}
+    ]`;
+  }
+
+  return `{
+    ${replaceInTemplate(mongooseKeyValTmpl, {
+      key: "type",
+      value: getType(type),
+    })}
+}`;
+}
+
+// get primitive types
 function getType(type) {
   switch (type) {
     case "string":
       return "String";
+    case "integer":
     case "number":
       return "Number";
+    case "boolean":
+      return "Boolean";
   }
 }
+
+module.exports = mongooseTransformer;
